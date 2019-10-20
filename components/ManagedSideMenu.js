5364fe19d471af69356f35ec236104fbcfe03fc8
Arman Ghazi <arman@Armans-MacBook-Pro.local>
2019-10-19 19:44:35 -0700

    Side menu ui and refactoring updatees

--- components/ManagedSideMenu.js
+++ components/ManagedSideMenu.js
@@ -2,59 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Alert, Image } from 'react-native';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
 import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import COLORS from './Colors';
+import { loadUser } from './Functions';
 
 export class ManagedSideMenu extends Component {
 	
 	constructor(props) {
 		super(props);
-    this.state = {
-      name:'',
-      rating:'',
-      trainerState: 'null',
-      image: 'null',
-      active: ''
-    }
+    this.state = {}
 	}
 
-	componentDidMount(){
-		Font.loadAsync({
-  		FontAwesome: require('../fonts/font-awesome-4.7.0/fonts/FontAwesome.otf'),
-  		fontAwesome: require('../fonts/font-awesome-4.7.0/fonts/fontawesome-webfont.ttf'),
-  	});
-    //pull user from database and check if trainer
-    let userId = firebase.auth().currentUser.uid;
-    let usersRef = firebase.database().ref(`/users/${userId}`);
-    usersRef.once("value", function(snapshot) {
-      var currentUser = snapshot.val();
-      var trainerState = currentUser.trainer;
-      var active;
-      if(trainerState){
-        active = currentUser.active;
-      }
-      firebase.storage().ref().child(userId).getDownloadURL().then(function(url){
-        this.setState({
-          image: url,
-          trainerState: trainerState,
-          name: currentUser.name,
-          rating: currentUser.rating,
-          active: active,
-        });
-      }.bind(this), function(error){
-        this.setState({
-          trainerState: trainerState,
-          name: currentUser.name,
-          rating: currentUser.rating,
-          active: active
-        });
-      }.bind(this));
-    }.bind(this));
+	async componentDidMount(){
+    const user = await loadUser(firebase.auth().currentUser.uid);
+    this.setState({ user });
 	}
 
-    // user log out confirm
   logout() {
     Alert.alert(
       "Are you sure you wish to sign out?", 
@@ -73,70 +37,63 @@ export class ManagedSideMenu extends Component {
   }
 
 	render(){
-    if(this.state.trainerState == 'null'){
+    if(!this.state.user){
       return <AppLoading />
-    }else{
-      var clientLink;
-      var active;
-      if(this.state.trainerState){
-        clientLink = (
-          <TouchableOpacity onPress={() => Actions.ClientPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Clients</Text>
-            </Text>
-          </TouchableOpacity>
-        );
-        if(this.state.active){
-          active = (<Text style={{fontSize: 20, color: COLORS.WHITE}}>Active</Text>);
-        }else{
-          active = (<Text style={{fontSize: 20, color: COLORS.RED}}>Away</Text>)
-        }
-      }else{
-        clientLink = (
-          <TouchableOpacity onPress={() => Actions.TrainerPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Trainers</Text>
-            </Text>
-          </TouchableOpacity>
-        );
-      }
-    	return(
-    		<View style={styles.container}>
-          <View style={styles.nameContainer}>
-            <View style={styles.imageContainer}>
-              <Image style={styles.image} source={{ uri: this.state.image }} />
-            </View>
-            <View style={styles.infoContainer}>
-              <Text style={{fontSize: 25, color: COLORS.WHITE}}>{this.state.name}</Text>
-              <Text style={{fontSize: 20, color: COLORS.WHITE}}>Rating: {this.state.rating}</Text>
-              {active}
-            </View>
-          </View>
-          <TouchableOpacity onPress={() => Actions.MapPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.compass}</FontAwesome> <Text style={styles.menuLink}> Map</Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={styles.icon} onPress={() => Actions.SettingsPage({trainer: true})}>
-                <FontAwesome>{Icons.gear}</FontAwesome> <Text style={styles.menuLink}> Settings</Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Actions.CalendarPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.calendar}</FontAwesome> <Text style={styles.menuLink}> Calendar</Text>
-            </Text>
-          </TouchableOpacity>
-          {clientLink}
-          <TouchableOpacity onPress={this.logout}>
-            <Text style={styles.icon}>
-              <FontAwesome>{Icons.powerOff}</FontAwesome> <Text style={styles.menuLink}> Sign Out</Text>
-            </Text>
-          </TouchableOpacity>
-        </View>
-    		)
     }
-	}
+    let clientLink, active;
+    if(this.state.user.trainer){
+      clientLink = (
+        <TouchableOpacity onPress={() => Actions.ClientPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Clients</Text>
+          </Text>
+        </TouchableOpacity>
+      );
+      if(this.state.user.active){
+        active = (<Text style={{fontSize: 20, color: COLORS.WHITE}}>Active</Text>);
+      }else{
+        active = (<Text style={{fontSize: 20, color: COLORS.RED}}>Away</Text>)
+      }
+    }else{
+      clientLink = (
+        <TouchableOpacity onPress={() => Actions.TrainerPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Trainers</Text>
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+    return(
+      <View style={styles.container}>
+        <View style={styles.nameContainer}>
+          <Text style={{fontSize: 25, color: COLORS.WHITE}}>{this.state.user.name}</Text>
+          <Text style={{fontSize: 20, color: COLORS.WHITE}}>Rating: {this.state.user.rating}</Text>
+          {active}
+        </View>
+        <TouchableOpacity onPress={() => Actions.MapPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.compass}</FontAwesome> <Text style={styles.menuLink}> Map</Text>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.icon} onPress={() => Actions.SettingsPage({trainer: true})}>
+              <FontAwesome>{Icons.gear}</FontAwesome> <Text style={styles.menuLink}> Settings</Text>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Actions.CalendarPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.calendar}</FontAwesome> <Text style={styles.menuLink}> Calendar</Text>
+          </Text>
+        </TouchableOpacity>
+        {clientLink}
+        <TouchableOpacity onPress={this.logout}>
+          <Text style={styles.icon}>
+            <FontAwesome>{Icons.powerOff}</FontAwesome> <Text style={styles.menuLink}> Sign Out</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -148,21 +105,13 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.WHITE
 	},
   nameContainer: {
-    height: 140,
     width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: COLORS.SECONDARY,
-    padding: 15,
-    paddingTop: 40
-  },
-  infoContainer: {
-    height: 100,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    padding: 15,
+    paddingBottom: 20,
+    paddingTop: 30
   },
   icon:{
     fontSize: 30,
@@ -173,15 +122,5 @@ const styles = StyleSheet.create({
   menuLink:{
     fontSize: 30,
     color: COLORS.PRIMARY
-  },
-  imageContainer: {
-    height: 80,
-    width: 80,
-    borderRadius: 40
-  },
-  image: {
-    height: 80,
-    width: 80,
-    borderRadius: 40
   }
 });
--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,57 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
 import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import COLORS from './Colors';
+import { loadUser } from './Functions';
 
 export class SideMenu extends Component {
 	
 	constructor(props) {
 		super(props);
-    this.state = {
-      name:'',
-      rating:'',
-      trainerState: 'null',
-      image: 'null',
-      active: ''
-    }
+    this.state = {};
 	}
 
-	componentDidMount(){
-		Font.loadAsync({
-  		FontAwesome: require('../fonts/font-awesome-4.7.0/fonts/FontAwesome.otf'),
-  		fontAwesome: require('../fonts/font-awesome-4.7.0/fonts/fontawesome-webfont.ttf'),
-  	});
-    //pull user from database and check if trainer
-    let userId = firebase.auth().currentUser.uid;
-    let usersRef = firebase.database().ref(`/users/${userId}`);
-    usersRef.once("value", function(snapshot) {
-      var currentUser = snapshot.val();
-      var trainerState = currentUser.trainer;
-      var active;
-      if(trainerState){
-        active = currentUser.active;
-      }
-      firebase.storage().ref().child(userId).getDownloadURL().then(function(url){
-        this.setState({
-          image: url,
-          trainerState: trainerState,
-          name: currentUser.name,
-          rating: currentUser.rating,
-          active: active,
-        });
-      }.bind(this), function(error){
-        this.setState({
-          trainerState: trainerState,
-          name: currentUser.name,
-          rating: currentUser.rating,
-          active: active
-        });
-      }.bind(this));
-    }.bind(this));
+	async componentDidMount(){
+    const user = await loadUser(firebase.auth().currentUser.uid);
+    this.setState({ user });
 	}
 
     // user log out confirm
@@ -73,81 +38,74 @@ export class SideMenu extends Component {
   }
 
 	render(){
-    if(this.state.trainerState == 'null'){
+    if(!this.state.user){
       return <AppLoading />
-    }else{
-      var clientLink;
-      var active;
-      if(this.state.trainerState){
-        clientLink = (
-          <TouchableOpacity onPress={() => Actions.ClientPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Clients</Text>
-            </Text>
-          </TouchableOpacity>
-        );
-        if(this.state.active){
-          active = (<Text style={{fontSize: 20, color: COLORS.WHITE}}>Active</Text>);
-        }else{
-          active = (<Text style={{fontSize: 20, color: COLORS.RED}}>Away</Text>)
-        }
-      }else{
-        clientLink = (
-          <TouchableOpacity onPress={() => Actions.TrainerPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Trainers</Text>
-            </Text>
-          </TouchableOpacity>
-        );
-      }
-
-    	return(
-    		<View style={styles.container}>
-          <View style={styles.nameContainer}>
-            <View style={styles.imageContainer}>
-              <Image style={styles.image} source={{ uri: this.state.image }} />
-            </View>
-            <View style={styles.infoContainer}>
-              <Text style={{fontSize: 25, color: COLORS.WHITE}}>{this.state.name}</Text>
-              <Text style={{fontSize: 20, color: COLORS.WHITE}}>Rating: {this.state.rating}</Text>
-              {active}
-            </View>
-          </View>
-          <TouchableOpacity onPress={() => Actions.MapPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.compass}</FontAwesome> <Text style={styles.menuLink}> Map</Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={styles.icon} onPress={() => Actions.SettingsPage({trainer: this.state.trainerState})}>
-                <FontAwesome>{Icons.gear}</FontAwesome> <Text style={styles.menuLink}> Settings</Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Actions.CalendarPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.calendar}</FontAwesome> <Text style={styles.menuLink}> Calendar</Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Actions.HistoryPage()}>
-            <Text style={styles.icon}>
-                <FontAwesome>{Icons.list}</FontAwesome> <Text style={styles.menuLink}> History</Text>
-            </Text>
-          </TouchableOpacity>
-          {clientLink}
-          <TouchableOpacity onPress={() => Actions.PaymentPage()}>
-            <Text style={styles.icon}>
-              <FontAwesome>{Icons.creditCard}</FontAwesome> <Text style={styles.menuLink}> Payments</Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.logout}>
-            <Text style={styles.icon}>
-              <FontAwesome>{Icons.powerOff}</FontAwesome> <Text style={styles.menuLink}> Sign Out</Text>
-            </Text>
-          </TouchableOpacity>
-        </View>
-    		)
     }
-	}
+    let clientLink, active;
+    if(this.state.user.trainer){
+      clientLink = (
+        <TouchableOpacity onPress={() => Actions.ClientPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Clients</Text>
+          </Text>
+        </TouchableOpacity>
+      );
+      if(this.state.user.active){
+        active = (<Text style={{fontSize: 20, color: COLORS.WHITE}}>Active</Text>);
+      }else{
+        active = (<Text style={{fontSize: 20, color: COLORS.RED}}>Away</Text>)
+      }
+    }else{
+      clientLink = (
+        <TouchableOpacity onPress={() => Actions.TrainerPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.users}</FontAwesome> <Text style={styles.menuLink}> Trainers</Text>
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+
+    return(
+      <View style={styles.container}>
+        <View style={styles.nameContainer}>
+          <Text style={{fontSize: 25, color: COLORS.WHITE}}>{this.state.user.name}</Text>
+          <Text style={{fontSize: 20, color: COLORS.WHITE}}>Rating: {this.state.user.rating}</Text>
+          {active}
+        </View>
+        <TouchableOpacity onPress={() => Actions.MapPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.compass}</FontAwesome> <Text style={styles.menuLink}> Map</Text>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.icon} onPress={() => Actions.SettingsPage({trainer: this.state.user.active})}>
+              <FontAwesome>{Icons.gear}</FontAwesome> <Text style={styles.menuLink}> Settings</Text>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Actions.CalendarPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.calendar}</FontAwesome> <Text style={styles.menuLink}> Calendar</Text>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Actions.HistoryPage()}>
+          <Text style={styles.icon}>
+              <FontAwesome>{Icons.list}</FontAwesome> <Text style={styles.menuLink}> History</Text>
+          </Text>
+        </TouchableOpacity>
+        {clientLink}
+        <TouchableOpacity onPress={() => Actions.PaymentPage()}>
+          <Text style={styles.icon}>
+            <FontAwesome>{Icons.creditCard}</FontAwesome> <Text style={styles.menuLink}> Payments</Text>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.logout}>
+          <Text style={styles.icon}>
+            <FontAwesome>{Icons.powerOff}</FontAwesome> <Text style={styles.menuLink}> Sign Out</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -159,21 +117,13 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.WHITE
 	},
   nameContainer: {
-    height: 140,
     width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: COLORS.SECONDARY,
-    padding: 15,
-    paddingTop: 40
-  },
-  infoContainer: {
-    height: 100,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    padding: 15,
+    paddingBottom: 20,
+    paddingTop: 30
   },
   icon:{
     fontSize: 30,
@@ -185,14 +135,4 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: COLORS.PRIMARY
   },
-  imageContainer: {
-    height: 80,
-    width: 80,
-    borderRadius: 40
-  },
-  image: {
-    height: 80,
-    width: 80,
-    borderRadius: 40
-  }
 });
